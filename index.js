dc4e19838a1aa2f0a0344dd05e5157a37e97b573
Dwight Rodriques <drodriq1@its.jnj.com>
2018-08-19 23:07:47 -0400

    Initial commit

--- index.js
+++ index.js
@@ -26,10 +26,13 @@ export function httpErrorHandler (err, req, res, next) {
     return next(err)
   }
 
-  return res.json(
-    err.status,
-    {
-      message: err.message || STATUS_TEXTS[err.status]
-    }
-  )
+  res.json(err.status, {message: err.message || STATUS_TEXTS[err.status]})
 }
+
+// Todo how to use promises instead for try catch block
+// app.get("/", function (req, res, next) {
+//   Promise.resolve().then(function () {
+//     throw new Error("BROKEN");
+//   }).catch(next); // Errors will be passed to Express.
+// });
+
